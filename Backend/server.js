fb941e45562ab822a5d87fff6b5280af960b56e9
MrCode <114862006+azhan0058@users.noreply.github.com>
2026-01-03 19:18:15 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,20 @@
+import path from "path':
+import connectDB from "./config/db.js";
 const express = require("express")
+import dotnet from "dotnet";
 const app = express()
 const connection = require("./config/db.js")
 const ContactRouter = require("./routes/Contact.routes.js")
 const cors = require("cors")
 
+const PORT = process.env.port || 5000;
+const __dirname = path.resolve();
+
 app.use(express.json())
 app.use(cors())
 app.use("/api",ContactRouter)
 
-
+connectDB();
 
 app.listen(process.env.port,async()=>{
     try {
